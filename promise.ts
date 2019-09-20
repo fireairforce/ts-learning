5f81439dfcfc2a98b5491983877e7702f822e2d4
fireairforce <1344492820@qq.com>
2019-09-20 18:05:06 +0800

    feat:add promise

--- promise.ts
+++ promise.ts
@@ -1,9 +1,33 @@
-const promise = new Promise((resolve,reject)=>{
-    resolve(23333);
-    reject(new Error('this is a Error'));
-})
+// 使用setTimeout模拟像服务器端请求数据
 
-promise.then(res=>{
+function fetchUserInfo(userId: string): Promise<{}> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({ userId });
+    }, 1000);
+  });
+}
+
+function fetchCartInfo(userId: string): Promise<{}> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({ userId });
+    }, 1200);
+  });
+}
+
+function fetchGoodInfo(userId: string): Promise<{}> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({ userId });
+    }, 1400);
+  });
+}
+
+Promise.all([fetchUserInfo("1"), fetchCartInfo("2"), fetchGoodInfo("3")]).then(
+  res => {
     console.log(res);
-})
+  }
+);
 
+// 编译使用 tsc promise.ts --target es2015来进行编译
